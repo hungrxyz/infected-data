79c50e8384957323fd141cf3aba0fc95ad0520ee
marko <marko@hungry.dev>
2021-01-29 15:32:55 +0100

    Updates Vaccinations

--- Sources/Scalpel/Scalpel.swift
+++ Sources/Scalpel/Scalpel.swift
@@ -147,8 +147,8 @@ struct Scalpel: ParsableCommand {
             percentageOfPopulation: nil
         )
 
-        let previousVaccinationsTotal = 185_356
-        let currentVaccinationsTotal = 203_450
+        let previousVaccinationsTotal = 203_450
+        let currentVaccinationsTotal = 215_498
         let newVaccinations = currentVaccinationsTotal - previousVaccinationsTotal
         let percentageVaccinations = Float(currentVaccinationsTotal) / Float(nationalPopulation)
         let nationalVaccinationsPer100K = per100k(number: currentVaccinationsTotal, population: nationalPopulation)
